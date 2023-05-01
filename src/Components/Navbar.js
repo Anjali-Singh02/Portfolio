386b3b56fc90b303be9affdd4672f8097fd7af5a
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-01 23:56:07 +0530

    changed control icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,34 +1,60 @@
-import React, { Fragment, useState } from 'react'
-import { VscArrowCircleLeft } from "react-icons/vsc";
-import { HiOutlineHome } from "react-icons/hi";
+import React, { Fragment, useState } from 'react';
+import { TbCircleChevronLeft } from 'react-icons/tb';
+import { HiOutlineHome } from 'react-icons/hi';
 import { IconContext } from 'react-icons/lib';
 
 export const Navbar = () => {
-    const [open, setOpen] = useState(true)
-  return (
-    <Fragment>
-        
-    <div className='flex'>
-        <div className={`${open ? "w-64" : "w-20"} p-5 pt-8 h-screen bg-purple-950 text-white relative duration-300`}>
-              <IconContext.Provider value={{size:'2em', className:'global-class-name absolute cursor-pointer text-white duration-200  -right-3 rounded-full bg-purple-50 top-9 text-purple-950'}}>
+	const [open, setOpen] = useState(true);
+	return (
+		<Fragment>
+			{/* navbar container */}
+			<div className="flex">
+				{/* sidebar container */}
+				<div
+					className={`${
+						open ? 'w-64' : 'w-20'
+					} p-5 pt-8 h-screen bg-purple-950 text-white relative duration-300`}
+				>
+					{/* control icon wrapped inside IconContext.Provider */}
+					<IconContext.Provider
+						value={{
+							size: '2em',
+							className: `${
+								!open && 'rotate-180'
+							} global-class-name absolute cursor-pointer text-white duration-200  -right-3 rounded-full bg-purple-50 top-9 text-purple-950`,
+						}}
+					>
+						{/* control icon for side bar */}
+						<TbCircleChevronLeft onClick={() => setOpen(!open)} />
+					</IconContext.Provider>
+					{/* contents of side bar */}
+					<div className="flex gap-x-2 items-center">
+						<div>
+							<IconContext.Provider
+								value={{
+									size: '2em',
+									className:
+										'global-class-name  cursor-pointer text-white duration-200 items-center',
+								}}
+							>
+								<HiOutlineHome />
+							</IconContext.Provider>
+						</div>
 
-            <VscArrowCircleLeft onClick = {() => setOpen(!open)} className={`${!open && "rotate-180"}`}/>
-              </IconContext.Provider>
-            <div className='flex gap-x-2 items-center'>
-             <div className=' '>
-                <IconContext.Provider value={{size:'2em', className:'global-class-name  cursor-pointer text-white duration-200 items-center'}}>
-           <HiOutlineHome />
-                </IconContext.Provider>
-           </div>
-           <div>
-           <h3 className={`origin-left font-medium text-xl duration-100 ${!open && "scale-0"}`}>Home</h3>
-        </div> 
-        </div>
-        </div>
-
-        <div>Home Page</div>
-    </div>
-
-    </Fragment>
-  )
-}
+						<div>
+							<h3
+								className={`origin-left font-medium text-xl duration-100 ${
+									!open && 'scale-0'
+								}`}
+							>
+								Home
+							</h3>
+						</div>
+					</div>
+				</div>
+				{/* Content container starts here */}
+				<div>Home Page</div>
+			</div>
+		</Fragment>
+	);
+};
