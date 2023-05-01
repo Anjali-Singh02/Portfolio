bb6e49434606dc51a0e8dbf6ca2982fb8cecca10
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-01 23:31:19 +0530

    added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
    <Fragment>
-      
+      <Navbar/>
    </Fragment>
   );
 }
