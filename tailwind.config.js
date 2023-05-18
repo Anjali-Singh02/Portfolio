6d7d38513979584078fae55f31afb34e7e269e83
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-18 23:52:10 +0530

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
+			colors: {
+				purple: '#240046',
+				golden: '#d08d03',
+			},
 			keyframes: {
 				wiggle: {
 					'0%, 100%': { transform: 'rotate(-3deg)' },
