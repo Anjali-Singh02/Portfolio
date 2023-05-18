1ff80de860ca9c4a9ed0b0757557d948be5841e2
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-18 23:50:20 +0530

    applied DRY and updated contents using array and mapped objects

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,33 +7,88 @@ import { MdContacts } from 'react-icons/md';
 import { BsQuestionCircleFill } from 'react-icons/bs';
 import { VscGithub } from 'react-icons/vsc';
 import { IconContext } from 'react-icons/lib';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Education from '../pages/Education';
 import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
 import Footer from './Footer';
+import NavLinks from './NavLinks';
 
 export const Navbar = () => {
 	const [open, setOpen] = useState(true);
+
+	const navLinks = [
+		{
+			title: 'Home',
+			link: '/',
+			Component: AiFillHome,
+			style: 'hover:text-golden ',
+		},
+		{
+			title: 'About',
+			link: '/about',
+			Component: BsQuestionCircleFill,
+			style: 'hover:text-golden',
+		},
+		{
+			title: 'Education',
+			link: '/education',
+			Component: FaUserGraduate,
+			style: 'hover:text-golden',
+		},
+		{
+			title: 'projects',
+			link: '/projects',
+			Component: FaChartLine,
+			style: 'hover:text-golden',
+		},
+		{
+			title: 'contacts',
+			link: '/contact',
+			Component: MdContacts,
+			style: 'hover:text-golden',
+		},
+	];
+
+	const socialLinks = [
+		{
+			link: 'https://www.linkedin.com/in/anjali-singh02/',
+			Component: AiOutlineLinkedin,
+		},
+		{
+			link: 'https://github.com/Anjali-Singh02',
+			Component: VscGithub,
+		},
+		{
+			link: 'https://www.instagram.com/anju01173/',
+			Component: FaInstagram,
+		},
+		{
+			link: 'https://twitter.com/Anjali__Singh02',
+			Component: TfiTwitter,
+		},
+	];
+
 	return (
 		<Fragment>
 			{/* navbar container */}
-			<div className="flex h-screen w-80">
+			<div className="flex min-h-screen h-full w-80">
 				{/* sidebar container */}
 				<div
 					className={`${
 						open ? 'w-8/12 ' : 'w-20 '
-					} p-3 pt-5 h-screen bg-purple-950 text-white relative duration-300 `}
+					} p-3 pt-5 h-screen bg-purple text-white relative transition-all ease-in duration-500 `}
 				>
 					{/* control icon wrapped inside IconContext.Provider */}
 					<IconContext.Provider
 						value={{
 							size: '2em',
+
 							className: `${
 								!open && 'rotate-180'
-							} global-class-name absolute cursor-pointer text-white duration-200  -right-3 rounded-full  top-9 bg-purple-950`,
+							} global-class-name absolute cursor-pointer text-white transition-all duration-700 -right-3 rounded-full  top-9 bg-purple  hover:visible`,
 						}}
 					>
 						{/* control icon for side bar */}
@@ -46,168 +101,49 @@ export const Navbar = () => {
 							alt="myImage"
 							className={`${
 								open &&
-								'rounded-2xl items-center h-60 w-44 m-auto cursor-pointer'
+								'rounded-2xl items-center h-60 w-44 m-auto cursor-pointer '
 							} ${
 								!open && 'h-14 w-14 m-auto  rounded-full mt-4'
 							} `}
 						/>
 					</div>
-					<div className={`flex-col mt-8 text-sm ${!open && 'pt-2'}`}>
-						<div className="flex text-center m-2">
-							<div>
-								<IconContext.Provider
-									value={{
-										size: '2em',
-										className:
-											'global-class-name p-1 cursor-pointer text-white duration-200 items-center',
-									}}
-								>
-									<Link to="/">
-										<AiFillHome />
-									</Link>
-								</IconContext.Provider>
-							</div>
-
-							<div>
-								<h3
-									className={`origin-left px-2 font-medium text-xl duration-100 ${
-										!open && 'scale-0'
-									}`}
-								>
-									<Link to="/">Home</Link>
-								</h3>
-							</div>
-						</div>
-						<div className="flex m-2">
-							<div>
-								<IconContext.Provider
-									value={{
-										size: '2em',
-										className:
-											'global-class-name p-1 cursor-pointer text-white duration-200 items-center',
-									}}
-								>
-									<Link to="/about">
-										<BsQuestionCircleFill />
-									</Link>
-								</IconContext.Provider>
-							</div>
-
-							<div>
-								<h3
-									className={`origin-left px-2 font-medium text-xl duration-100 ${
-										!open && 'scale-0'
-									}`}
-								>
-									<Link to="/about">About</Link>
-								</h3>
-							</div>
-						</div>
-						<div className="flex m-2">
-							<div>
-								<IconContext.Provider
-									value={{
-										size: '2em',
-										className:
-											'global-class-name p-1 cursor-pointer text-white duration-200 items-center',
-									}}
-								>
-									<Link to="/education">
-										<FaUserGraduate />
-									</Link>
-								</IconContext.Provider>
-							</div>
-
-							<div>
-								<h3
-									className={`origin-left px-2 font-medium text-xl duration-100 ${
-										!open && 'scale-0'
-									}`}
-								>
-									<Link to="/education">Education</Link>
-								</h3>
-							</div>
-						</div>
-						<div className="flex m-2">
-							<div>
-								<IconContext.Provider
-									value={{
-										size: '2em',
-										className:
-											'global-class-name p-1  cursor-pointer text-white duration-200 items-center',
-									}}
-								>
-									<Link to="/projects">
-										<FaChartLine />
-									</Link>
-								</IconContext.Provider>
-							</div>
-
-							<div>
-								<h3
-									className={`origin-left px-2 font-medium text-xl duration-100 ${
-										!open && 'scale-0'
-									}`}
-								>
-									<Link to="/projects">Projects</Link>
-								</h3>
-							</div>
-						</div>
-						<div className="flex m-2">
-							<div>
-								<IconContext.Provider
-									value={{
-										size: '2em',
-										className:
-											'global-class-name p-1 cursor-pointer text-white duration-200 items-center',
-									}}
-								>
-									<Link to="/contact">
-										<MdContacts />
-									</Link>
-								</IconContext.Provider>
-							</div>
-
-							<div>
-								<h3
-									className={`origin-left px-2 font-medium text-xl duration-100 ${
-										!open && 'scale-0'
-									}`}
-								>
-									<Link to="/contact">Contact</Link>
-								</h3>
-							</div>
-						</div>
+					<div
+						className={`flex-col mt-8 ml-3 text-sm uppercase ${
+							!open && 'pt-2 ml-1'
+						}`}
+					>
+						{navLinks.map((element, key) => {
+							return (
+								<NavLinks
+									props={{ open, ...element }}
+									key={key}
+								/>
+							);
+						})}
 					</div>
 					<div
 						className={` flex mt-52 pt-2   justify-evenly text-xl ${
 							!open && ' h-40 flex-col pl-4 mt-60'
 						}`}
 					>
-						<div className="">
-							<Link to="https://www.linkedin.com/in/anjali-singh02/">
-								<AiOutlineLinkedin />
-							</Link>
-						</div>
-						<div>
-							<Link to="https://github.com/Anjali-Singh02">
-								<VscGithub />
-							</Link>
-						</div>
+						{socialLinks.map((element, key) => {
+							const { link, Component } = element;
 
-						<div>
-							<Link to="https://www.instagram.com/anju01173/">
-								<FaInstagram />
-							</Link>
-						</div>
-						<div>
-							<Link to="https://twitter.com/Anjali__Singh02">
-								<TfiTwitter />
-							</Link>
-						</div>
+							return (
+								<div
+									className="motion-safe:hover:-translate-x-0.5 motion-safe:transition ... hover:text-golden"
+									key={key}
+								>
+									<Link to={link}>
+										<Component />
+									</Link>
+								</div>
+							);
+						})}
 					</div>
 				</div>
-				{/* Content container starts here */}
+
+				{/* Content container routing starts here */}
 				<div>
 					<Routes>
 						<Route path="/" element={<Home />} exact />
