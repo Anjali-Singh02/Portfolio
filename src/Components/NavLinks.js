fe5b1da3352976fce244d1437073630f5cd7af9c
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-20 21:59:18 +0530

    modified css property

--- src/Components/NavLinks.js
+++ src/Components/NavLinks.js
@@ -4,26 +4,25 @@ import { Link } from 'react-router-dom';
 
 const NavLinks = ({ props }) => {
 	console.log(props);
-	const { open, title, link, style, Component } = props;
+	const { open, title, link, Component } = props;
 	return (
 		<div
-			className={`flex text-center m-2 motion-safe:hover:-translate-x-0.5 motion-safe:transition ... duration-500 ${style}`}
+			className={`flex text-center m-2  motion-safe:hover:-translate-x-0.5 motion-safe:transition ... duration-500 `}
 		>
-			<div className={` flex `}>
+			<div className={` flex hover:text-golden`}>
 				<IconContext.Provider
 					value={{
 						size: '2em',
 						className:
-							'global-class-name p-1 cursor-pointer text-white duration-200 items-center hover:text-golden',
+							'global-class-name p-1 cursor-pointer  duration-200 items-center ',
 					}}
 				>
 					<Link to={link}>
 						<Component />
 					</Link>
 				</IconContext.Provider>
-
 				<h3
-					className={`origin-left px-2 font-medium text-sm transition-all ease-in duration-500  ${
+					className={`origin-left px-2 font-medium  text-sm transition-all ease-in duration-500  ${
 						!open && 'scale-0'
 					}  `}
 				>
