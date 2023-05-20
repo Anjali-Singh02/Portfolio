eef01bbac7ac37118f94f36a3767e2ca83d3380b
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-20 22:45:56 +0530

    imported SetPath component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,15 +7,9 @@ import { MdContacts } from 'react-icons/md';
 import { BsQuestionCircleFill } from 'react-icons/bs';
 import { VscGithub } from 'react-icons/vsc';
 import { IconContext } from 'react-icons/lib';
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Education from '../pages/Education';
-import Projects from '../pages/Projects';
-import Contact from '../pages/Contact';
-import Footer from './Footer';
 import NavLinks from './NavLinks';
 import SocialLinks from './SocialLinks';
+import SetPath from './SetPath';
 
 export const Navbar = () => {
 	const [open, setOpen] = useState(true);
@@ -25,31 +19,26 @@ export const Navbar = () => {
 			title: 'Home',
 			link: '/',
 			Component: AiFillHome,
-			style: 'hover:text-golden ',
 		},
 		{
 			title: 'About',
 			link: '/about',
 			Component: BsQuestionCircleFill,
-			style: 'hover:text-golden',
 		},
 		{
 			title: 'Education',
 			link: '/education',
 			Component: FaUserGraduate,
-			style: 'hover:text-golden',
 		},
 		{
 			title: 'projects',
 			link: '/projects',
 			Component: FaChartLine,
-			style: 'hover:text-golden',
 		},
 		{
 			title: 'contacts',
 			link: '/contact',
 			Component: MdContacts,
-			style: 'hover:text-golden',
 		},
 	];
 
@@ -75,84 +64,75 @@ export const Navbar = () => {
 	return (
 		<Fragment>
 			{/* navbar container */}
-			<div className="flex min-h-screen h-full w-80 transition-all ease-in duration-500">
-				{/* sidebar container */}
-				<div
-					className={`${
-						open ? 'w-8/12 ' : 'w-20 '
-					} p-3 pt-5 h-screen bg-purple text-white relative transition-all ease-in duration-500 `}
-				>
-					{/* control icon wrapped inside IconContext.Provider */}
-					<IconContext.Provider
-						value={{
-							size: '2em',
-
-							className: `${
-								!open && 'rotate-180'
-							} global-class-name absolute cursor-pointer text-white transition-all duration-700 -right-3 rounded-full  top-5 bg-purple  hover:visible`,
-						}}
-					>
-						{/* control icon for side bar */}
-						<TbCircleChevronLeft onClick={() => setOpen(!open)} />
-					</IconContext.Provider>
-					{/* contents of side bar */}
-					<div className="">
-						<img
-							src="/assets/profile.jpg"
-							alt="myImage"
-							className={`${
-								open &&
-								'rounded-2xl items-center h-60 w-44 m-auto cursor-pointer '
-							} ${
-								!open && 'h-14 w-14 m-auto  rounded-full mt-4'
-							} `}
-						/>
-					</div>
+			<div className="bg-gray-900 text-white">
+				<div className="flex min-h-screen h-full w-full transition-all ease-in duration-500">
+					{/* sidebar container */}
 					<div
-						className={`flex-col mt-8 ml-3 text-sm uppercase ${
-							!open && 'pt-2 ml-1'
-						}`}
+						className={`${
+							open ? 'w-60 ' : 'w-20 '
+						} p-3 pt-5 h-screen bg-gray-900 border-r-2 border-gray-500 relative transition-all ease-in duration-500 `}
 					>
-						{navLinks.map((element, key) => {
-							return (
-								<NavLinks
-									props={{ open, ...element }}
-									key={key}
-								/>
-							);
-						})}
-					</div>
-					<div
-						className={` flex mt-52 pt-2   justify-evenly text-xl ${
-							!open && ' h-40 flex-col pl-4 mt-60'
-						}`}
-					>
-						{socialLinks.map((element, key) => {
-							return (
-								<SocialLinks
-									props={{ open, ...element }}
-									key={key}
-								/>
-							);
-						})}
-					</div>
-				</div>
+						{/* control icon wrapped inside IconContext.Provider */}
+						<IconContext.Provider
+							value={{
+								size: '2em',
 
-				{/* Content container routing starts here */}
-				<div>
-					<Routes>
-						<Route path="/" element={<Home />} exact />
-						<Route path="/about" element={<About />} exact />
-						<Route path="/contact" element={<Contact />} exact />
-						<Route
-							path="/education"
-							element={<Education />}
-							exact
-						/>
-						<Route path="/projects" element={<Projects />} exact />
-					</Routes>
-					<div>
-						<Footer />
+								className: `${
+									!open && 'rotate-180'
+								} global-class-name absolute cursor-pointer text-white transition-all duration-700 -right-3 rounded-full  top-5 bg-purple  hover:visible`,
+							}}
+						>
+							{/* control icon for side bar */}
+							<TbCircleChevronLeft
+								onClick={() => setOpen(!open)}
+							/>
+						</IconContext.Provider>
+						{/* contents of side bar */}
+						<div className="">
+							<img
+								src="/assets/profile.jpg"
+								alt="myImage"
+								className={`${
+									open &&
+									'rounded-2xl items-center h-60 w-44 m-auto cursor-pointer '
+								} ${
+									!open &&
+									'h-14 w-14 m-auto  rounded-full mt-4'
+								} `}
+							/>
+						</div>
+						<div
+							className={`flex-col mt-8 ml-3 text-sm uppercase ${
+								!open && 'pt-2 ml-1'
+							}`}
+						>
+							{navLinks.map((element, key) => {
+								return (
+									<NavLinks
+										props={{ open, ...element }}
+										key={key}
+									/>
+								);
+							})}
+						</div>
+						<div
+							className={` flex mt-52 pt-2   justify-evenly text-xl ${
+								!open && ' h-40 flex-col pl-4 mt-60'
+							}`}
+						>
+							{socialLinks.map((element, key) => {
+								return (
+									<SocialLinks
+										props={{ open, ...element }}
+										key={key}
+									/>
+								);
+							})}
+						</div>
+					</div>
+					{/* Router component */}
+					<div className="w-1/2 m-auto mt-20 h-screen">
+						<SetPath />
 					</div>
 				</div>
 			</div>
