57c40b29ba61be27ca1d7a11174016604ea65f6d
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-20 22:48:32 +0530

    given width and set margin to auto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,24 @@ import About from './About';
 import Education from './Education';
 import Projects from './Projects';
 import Contact from './Contact';
-import Footer from '../Components/Footer';
 
 const Home = () => {
 	return (
-		<div>
-			Home
-			<About />
-			<Education />
-			<Projects />
-			<Contact />
+		<div className="m-auto   bg-green-500 text-center border-2">
+			<div className="h-52 p-3">Hi there! My name is Anjali Singh</div>
+
+			<div className="bg-cyan-700">
+				<About />
+			</div>
+			<div className="bg-emerald-700">
+				<Education />
+			</div>
+			<div className="bg-indigo-600">
+				<Projects />
+			</div>
+			<div className="bg-teal-700">
+				<Contact />
+			</div>
 		</div>
 	);
 };
